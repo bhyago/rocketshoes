f1f3c44ba11fb56a651b5b7dec3ce6656bfc5fa6
bhyago <bhyago@gmail.com>
2020-02-22 20:40:13 -0300

    removendo produto

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -8,7 +8,7 @@ import {
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -20,40 +20,45 @@ function Cart({ cart }) {
             <th>SUBTOTAL</th>
             <th />
           </tr>
-          <tbody>
-            {cart.map(product => (
-              <tr>
-                <td>
-                  <img src={product.image} alt={product.title} />
-                </td>
-                <td>
-                  <strong>{product.title}</strong>
-                  <span>{product.priceFormatted}</span>
-                </td>
-                <td>
-                  <div>
-                    <button type="button">
-                      <MdRemoveCircleOutline size={20} color="#7159c1" />
-                    </button>
-                    <input type="number" readOnly value={product.amount} />
-                    <button type="button">
-                      <MdAddCircleOutline size={20} color="#7159c1" />
-                    </button>
-                  </div>
-                </td>
-
-                <td>
-                  <strong>R$258,80</strong>
-                </td>
-                <td>
-                  <button type="button">
-                    <MdDelete size={20} color="#7159c1" />
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
         </thead>
+        <tbody>
+          {cart.map(product => (
+            <tr>
+              <td>
+                <img src={product.image} alt={product.title} />
+              </td>
+              <td>
+                <strong>{product.title}</strong>
+                <span>{product.priceFormatted}</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input type="number" readOnly value={product.amount} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+
+              <td>
+                <strong>R$258,80</strong>
+              </td>
+              <td>
+                <button
+                  type="button"
+                  onClick={() =>
+                    dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                  }
+                >
+                  <MdDelete size={20} color="#7159c1" />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </ProductTable>
 
       <footer>
